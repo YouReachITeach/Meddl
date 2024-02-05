f67f447f2f5b39e260d44d98cbddbe922e8f5b89
Victoria <victoria.weinelt@tum.de>
2024-02-05 11:30:30 +0100

    hihi

--- cacac.java
+++ cacac.java
@@ -3,5 +3,10 @@ public class cacac {
     public static void main(String[] args) {
         System.out.println("hi");
         System.out.println("hallo");
+
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println("hdl");
+        }
     }
 }
